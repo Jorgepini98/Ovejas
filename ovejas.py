f6ab2cb839b532a72073312143e20e7044e2e58f
jorge <jorgepinilla98@hotmail.com>
2022-05-10 15:43:47 +0200

    Teniendo en cuenta una situación de datos ya generados, analiza los datos presentes y evita generar datos repetidos. Tras verificarlo, el propio proceso de comparación de datos hace que tarde mas que volver a ejecutar todo de nuevo

--- ovejas.py
+++ ovejas.py
@@ -65,6 +65,65 @@ def isNum(char):
 
     return true
 
+def findPos(df, date, hour, min, sec):
+
+    length = df.shape[0]
+
+    x = 0
+
+    featurePos = -1
+
+    while (x < length):
+
+        #Sensor,Date,Hour,Minutes,Seconds,...
+
+        checkDate = df.iloc[x,1]
+
+        dateReached = 0
+        hourReached = 0
+        minReached = 0
+
+        if(checkDate == date):
+
+            dateReached = 1
+
+            checkHour = df.iloc[x,2]
+
+            if(str(checkHour) == hour):
+
+                hourReached = 1
+
+                checkMin = df.iloc[x,3]
+
+                if(str(checkMin) == min):
+
+                    minreached = 1
+
+                    checkSec = df.iloc[x,4]
+
+                    if(str(checkSec) == sec):
+
+                        featurePos = x
+
+                        x = length
+                        
+                else:
+                    if(minReached == 1):
+                        x = length
+                    
+            else:
+                if(hourReached == 1):
+                    x = length
+
+        else:
+            if(dateReached == 1):
+                x = length
+
+        x = x + 1
+
+    return featurePos
+
+
 def readSensorNum(path):
 
     counter = 0
@@ -93,7 +152,7 @@ def readSensorNum(path):
 
     return sensor
 
-def getFeatures(data):
+def getFeatures(data, dataFeatures):
 
     # Accel_x = data.iloc[:,2]
     # Accel_y = data.iloc[:,3]
@@ -107,23 +166,56 @@ def getFeatures(data):
     # Magnet_y = data.iloc[:,9]
     # Magnet_z = data.iloc[:,10]
 
-    acel = 0
-    gyro = 0
+    Acel_1 = 0
+    Acel_2 = 0
 
+    desiredColumns = ["Acel_1","Acel_2"] 
+    
+    columnsToProcess = desiredColumns
+    
+    currentColumns = dataFeatures.columns
+    
+    for x in range(len(currentColumns) - 1):
+        for y in range(len(desiredColumns) - 1):
+        
+            if str(currentColumns[x]) == desiredColumns[y]:
+                
+                columnsToProcess.pop(columnsToProcess.index(desiredColumns[y]))
+
+    
+   
     for y in range(data.shape[0]):
 
         if y != 0:
+        
+            if  "Acel_1" in columnsToProcess:
 
-            acel = acel + math.sqrt(pow(int(data.iloc[y,2]),2)) + math.sqrt(pow(int(data.iloc[y,3]),2)) + math.sqrt(pow(int(data.iloc[y,4]),2))
+                Acel_1 = Acel_1+ math.sqrt(pow(int(data.iloc[y,2]),2)) + math.sqrt(pow(int(data.iloc[y,3]),2)) + math.sqrt(pow(int(data.iloc[y,4]),2))
                     # print(acel)
+                    
+            else:
+                
+                Acel_1 = int(dataFeatures["Acel_1"])
+                
+            # print(Acel_1)
 
-    acel = acel/data.shape[0]
+            if  "Acel_2" in columnsToProcess:      
+            
+                Acel_2 = Acel_1
+                
+            else:
+                
+                Acel_2 = int(dataFeatures["Acel_2"])
 
-    features = pd.DataFrame({"Acel_1": [acel], "Acel_2": [acel]})
+            # print(Acel_2)
+
+
+
+    features = pd.DataFrame({Acel_1,Acel_2},columns = desiredColumns)
 
     return features
 
-def readDataFrame(path, file, df):
+def readDataFrame(path, file, df, sensor_features):
 
     count = 0
     
@@ -155,10 +247,21 @@ def readDataFrame(path, file, df):
             min = readMinutes(time)
             sec = readSeconds(time)
 
+            featurePos = findPos(sensor_features,date,hour,min,sec)
+
+            if(featurePos >= 0):
+
+                dataFeatures = pd.DataFrame(data = sensor_features.iloc[featurePos,:]).T
+
+            else:
+                dataFeatures = pd.DataFrame()
+
+            # print(dataFeatures)
+
             data = pd.DataFrame(df.loc[(x+1):(x+20)])
 
 
-            features = getFeatures(data)
+            features = getFeatures(data,dataFeatures)
 
             data = pd.DataFrame({'Sensor': [Sensor], 'Date': [date], 'Hour': [hour], 'Minutes': [min], "Seconds": [sec]})
 
@@ -172,7 +275,7 @@ def readDataFrame(path, file, df):
             if x == 0:
                 results = pd.DataFrame(data)
             else:
-                results = results.append(data, ignore_index=True)
+                results = results.append(data, ignore_index=True) #, ignore_index=True
                 # print(results)
 
     # print("resultados")
@@ -180,18 +283,29 @@ def readDataFrame(path, file, df):
 
     return results
 
-def readSensor(sensor, path1):
+def readSensor(sensor,  raw_path, features_path):
 
     print("SENSOR: ")
     print(sensor)
 
-    path2 = path1 + sensor + "/"
+    sensor_path = raw_path + sensor + "/"
 
-    files = os.listdir(path2)
+    files = os.listdir(sensor_path)
 
     content = pd.DataFrame()
 
-    # checking all the csv files in the
+    
+
+    try:
+
+        sensor_features = pd.read_csv(features_path + sensor + ".csv")
+        sensor_features = sensor_features.iloc[:, 1:]
+
+    except:
+        print("No hay .csv generado para el Sensor: "+sensor)
+        sensor_features = pd.DataFrame()
+
+    # checking all the csv files in theS
     # specified path
     for file in files:
 
@@ -201,28 +315,31 @@ def readSensor(sensor, path1):
 
         if file.endswith(".csv"):
 
-            df = pd.read_csv(path2 + file, sep=';', names = ['HORA','Timestamp','Accel_x','Accel_y','Accel_z','Gyro_x','Gyro_y','Gyro_z','Magnet_x','Magnet_y','Magnet_z'])
+            raw = pd.read_csv(sensor_path + file, sep=';', names = ['HORA','Timestamp','Accel_x','Accel_y','Accel_z','Gyro_x','Gyro_y','Gyro_z','Magnet_x','Magnet_y','Magnet_z'])
 
             if content.empty :
 
-                content = readDataFrame(path2, file, df)
+                content = readDataFrame(sensor_path, file, raw, sensor_features)
 
             else:
 
-                content = content.append(readDataFrame(path2, file, df),  ignore_index=True)
+                content = content.append(readDataFrame(sensor_path, file, raw, sensor_features), ignore_index=True) #, ignore_index=True
 
             # break
+            print(sensor +":  " + file)
 
     # print(content)
     content.to_csv('CSVs/features_' + str(sensor) + ".csv")
 
 
 
-path1 = 'PrimerasMedidas/'
+raw_path = 'PrimerasMedidas/'
+
+features_path = 'CSVs/features_'
 
 n_jobs = multiprocessing.cpu_count() - 1
 
-Parallel(n_jobs=n_jobs)(delayed(readSensor)(sensor, path1) for sensor in os.listdir(path1))
+Parallel(n_jobs=n_jobs)(delayed(readSensor)(sensor, raw_path, features_path) for sensor in os.listdir(raw_path))
 
 
 
